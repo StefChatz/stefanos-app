1044a5ed8ca92663428e6bf84664cc51adbe6e54
dimitrisafendras <dimitris.afendras@gmail.com>
2020-12-07 14:42:13 +0200

    header compontentization

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import './Header.css';
+import './Header.scss';
 
 export const Header = () => (
-		<div className='Header'/>
+	<div className='Header'>
+		<div className='Search'>
+			<div className='Logo'></div>
+			<div className='SearchBar'></div>
+		</div>
+		<div className='Carousel'></div>
+		<div className='Settings'></div>
+	</div>
 	);
