5117eae5d811041e376f5cad4bdec54068340830
stefchatz <sxatzakis@yahoo.gr>
2020-12-07 15:50:28 +0200

    addded apple logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.scss';
+import AppleIcon from '@material-ui/icons/Apple';
 
 export const Header = () => (
 	<div className='Header'>
 		<div className='Search'>
-			<div className='Logo'></div>
+			<div className='Logo'><AppleIcon className='Icon'/></div>
 			<div className='SearchBar'></div>
 		</div>
 		<div className='Carousel'></div>
